a2f8145a4102d2b8f4664792c90fe4b30ce6af25
lilingkang <872706089@qq.com>
2022-11-13 19:52:24 +0800

    Initial commit

---
+++ Source/UE4CplusLearning/UE4CplusLearningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4CplusLearningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4CPLUSLEARNING_API AUE4CplusLearningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
